dc792322920527ae0e81033e683f356ad1537928
James <jshand27@gmail.com>
2018-01-04 19:19:09 +0000

    added a funkier font, added a header component to include this, modded header styles and removed stock react boilerplate stuff

---
+++ src/Components/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return(
+      <div>
+        <div>
+          
+        </div>
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default About;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return(
+      <link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet"/>
+    );
+  }
+}
+
+export default Header;
