f776bb9c90124f3528f116b9fe3f008f635b7185
Josh Ghent <me@joshghent.com>
2018-01-05 00:11:28 +0000

    Moved componets into pages. Added new sample components to get the routes rendering

---
+++ src/Components/Game/GamesList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class GamesList extends Component {
+  render() {
+    return (
+      <div className="GamesList">
+      </div>
+    );
+  }
+}
+
+export default GamesList;
